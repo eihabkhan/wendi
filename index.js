1588a6d7ecf206bd924f170b85c35137f5094dda
Eihab <mastereihab@gmail.com>
2020-08-31 15:53:55 +0100

    added new cards and clear functionality

--- index.js
+++ index.js
@@ -17,20 +17,21 @@ let currentActiveCard = 0;
 const cardsElement = [];
 
 // Store Card Data
-const cardsData = [
-    {
-        question: "What is JS?",
-        answer:"A Programming Language"
-    },
-    {
-        question: "What is a variable?",
-        answer:"container for a value"
-    },
-    {
-        question: "What is a monkey?",
-        answer:"An animal"
-    },
-];
+let cardsData = getCardsData()
+// const cardsData = [
+//     {
+//         question: "What is JS?",
+//         answer:"A Programming Language"
+//     },
+//     {
+//         question: "What is a variable?",
+//         answer:"container for a value"
+//     },
+//     {
+//         question: "What is a monkey?",
+//         answer:"An animal"
+//     },
+// ];
 
 // Create Cards
 function createCards() {
@@ -71,6 +72,16 @@ function updateCurrentCard() {
     currentCardElement.innerText = `${currentActiveCard + 1} / ${cardsElement.length}`
 }
 
+function getCardsData() {
+    const cards = JSON.parse(localStorage.getItem("cards"));
+    return cards !== null ? cards : [];
+}
+
+function persistCardData(cards) {
+    localStorage.setItem("cards", JSON.stringify(cards))
+    window.location.reload();
+}
+
 createCards();
 
 nextBtn.addEventListener("click", () => {
@@ -94,3 +105,39 @@ prevBtn.addEventListener("click", () => {
     cardsElement[currentActiveCard].className = "card active"
     updateCurrentCard()
 })
+
+
+showBtn.addEventListener("click", () =>{
+    addContainer.classList.add("show");
+});
+
+hideBtn.addEventListener("click", () => {
+    addContainer.classList.remove("show");
+})
+
+addCardBtn.addEventListener("click", () => {
+    const question = questionElement.value
+    const answer = answerElement.value
+    console.log(question, answer);
+
+    if (question.trim() && answer.trim()) {
+        const card = { question, answer }
+        createCard(card);
+        
+        questionElement.value = ""
+        answerElement.value = ""
+        
+        addContainer.classList.remove("show");
+        cardsData.push(card)
+
+        persistCardData(cardsData)
+    }
+
+})
+
+// Clear Cards
+clearBtn.addEventListener("click", () => {
+    localStorage.clear();
+    cardsContainer.innerHTML = ""
+    window.location.reload();
+});
